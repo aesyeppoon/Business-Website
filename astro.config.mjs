37b7f8af12161fae9657386a6c522b877dee235a
Nick <nick@example.com>
2026-07-01 15:31:32 +1000

    Improve local SEO metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://adaptiveelectricalsolutions.com.au",
+  site: "https://www.adaptive-electrical.com.au",
   output: "static",
   integrations: [react()],
   build: {
